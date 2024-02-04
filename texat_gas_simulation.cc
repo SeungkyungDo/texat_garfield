166f30740eb864fe475d7057e537a9c0e175a5c2
Jung Woo Lee <phyjics@gmail.com>
2024-02-04 15:39:46 +0900

    texat_gas_simulation x range can be set

--- texat_gas_simulation.cc
+++ texat_gas_simulation.cc
@@ -75,17 +75,26 @@ class WireSet
 
 int main(int argc, char * argv[])
 {
-    if (argc<2) {
-        cout << "== Input should be given 1) number of electrons in each point, 2) spacing between electron simulation starting point!" << endl;
-        cout << "   ex) ./texat_gas_simulation 10 0.5" << endl;
+    if (argc<3) {
+        cout << "== Input should be given" << endl;
+        cout << "   1) number of electrons in each point," << endl;
+        cout << "   2) spacing between electron simulation starting point," << endl;
+        cout << "   3) x-range 1," << endl;
+        cout << "   4) x-range 2." << endl;
+        cout << "   ex) ./texat_gas_simulation  10  0.5  0  20" << endl;
         return 0;
     }
 
-    int numElSim = std::atoi(argv[1]);
+    int numElSim    = std::atoi(argv[1]);
     double xSpacing = std::atof(argv[2]);
-    const double xMin = 0.;
-    const double xMax = 20.;
-    cout << numElSim << " " << xSpacing << " " << xMin << " " << xMax << endl;
+    double xMin = 0.;
+    double xMax = 20.;
+    if (argc>4) {
+        xMin = std::atof(argv[3]);
+        xMax = std::atof(argv[4]);
+    }
+    const int numPoints = int((xMax - xMin) / xSpacing);
+    cout << numElSim << " " << xSpacing << " " << xMin << " " << xMax << " " << numPoints << endl;
 
     bool addGG1 = true; // 0.201 -260
     bool addGG2 = true; // 0.201 -230
@@ -168,20 +177,19 @@ int main(int argc, char * argv[])
     // Assuming the transfer function is named "TexAT_v2_transfer_function.txt".
     // readTransferFunction(sensor); // Implement this function or load the transfer function file.
 
-
     ///////////////////////////////////////////////////////////////////////Added by S. Bae 240124
     double xStart, yStart, zStart, tStart, eStart;   
     double xEnd, yEnd, zEnd, tEnd, eEnd;				
-    TFile* fnew =new TFile("output.root","RECREATE");
-    TTree* tr = new TTree("gg","");
-    tr->Branch("xStart",&xStart,"xStart/D");
-    tr->Branch("yStart",&yStart,"yStart/D");
-    tr->Branch("zStart",&zStart,"zStart/D");
-    tr->Branch("tStart",&tStart,"tStart/D");
-    tr->Branch("xEnd",&xEnd,"xEnd/D");
-    tr->Branch("yEnd",&yEnd,"yEnd/D");
-    tr->Branch("zEnd",&zEnd,"zEnd/D");
-    tr->Branch("tEnd",&tEnd,"tEnd/D");
+    TFile* fnew = new TFile(Form("output_%d_%d_%.1f_%.1f.root",numElSim,numPoints,xMin,xMax),"RECREATE");
+    TTree* tree = new TTree("gg","");
+    tree -> Branch("xStart",&xStart,"xStart/D");
+    tree -> Branch("yStart",&yStart,"yStart/D");
+    tree -> Branch("zStart",&zStart,"zStart/D");
+    tree -> Branch("tStart",&tStart,"tStart/D");
+    tree -> Branch("xEnd",&xEnd,"xEnd/D");
+    tree -> Branch("yEnd",&yEnd,"yEnd/D");
+    tree -> Branch("zEnd",&zEnd,"zEnd/D");
+    tree -> Branch("tEnd",&tEnd,"tEnd/D");
 
     //////////////////////////////////////////////////////////////////////
     for (int i = 0; i < numElSim; ++i)
@@ -206,17 +214,17 @@ int main(int argc, char * argv[])
                 if (std::abs(yEnd) < 0.1)
                     nElectronsCollected++;
 
-                tr->Fill();
+                tree -> Fill();
             }
         }
     }
 
     driftView.Plot(true, false);
-    can -> SaveAs("drift_electron.png");
+    can -> SaveAs("figure_drift_electron.png");
 
-    fnew->WriteTObject(tr);	//Added by S. Bae 240124
-    fnew->Print();			//Added by S. Bae 240124
-    fnew->Close();			//Added by S. Bae 240124
+    fnew -> WriteTObject(tree);	//Added by S. Bae 240124
+    fnew -> Print();			//Added by S. Bae 240124
+    fnew -> Close();			//Added by S. Bae 240124
 
     app.Run(kTRUE);
     return 0;
